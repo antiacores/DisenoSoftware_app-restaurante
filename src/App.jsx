7f5f94f75798cea8448ad3938cc268720956ab40
Antía Cores <antiacores@MacBook-Air-de-Antia.local>
2024-11-11 19:26:38 -0600

    auth system with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import { useState } from 'react'
 import './App.css'
-import { Menu } from './components/Menu'
+import { Menu } from './components/Menu.jsx'
+import { Orders } from './components/Orders.jsx'
+import { LoginForm } from './components/LoginForm.jsx'
+import { Header } from './components/Header.jsx'
 
 function App() {
-
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
   return (
     <>
-      <Menu />
-    </>
-  )
+      <Header 
+      isAdmin={isAdmin} 
+      setIsAdmin={setIsAdmin} 
+      isAuthenticated={isAuthenticated} 
+      setIsAuthenticated={setIsAuthenticated}/>
+
+      {isAuthenticated ? (
+        <div className='container mx-auto px-4'>
+          {isAdmin ? (
+            <div>
+              <h1 className='text-3xl font-bold mb-4'></h1>
+              <Orders />
+            </div>
+          ) : (
+              <Menu/>
+          )}
+              </div>
+            ) : (
+              <LoginForm
+                isAuthenticated={isAuthenticated}
+                setIsAuthenticated={setIsAuthenticated}
+              />
+        )}
+  </>       
+  );
 }
 
-export default App
+export default App;
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-export const Item = ({name, price, description}) => {
+export const Item = ({ name, price, description, onAddToCart }) => {
   return (
     <div>
         <h2>{name} - ${price}</h2>
         <h3>{description}</h3>
+        <button onClick={() => onAddToCart({ name, price, quantity: 1 })}>
+            Agregar al carrito
+        </button>
     </div>
-  )
-}
+  );
+};
